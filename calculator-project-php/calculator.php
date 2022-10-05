f7cd75168cf2805d03cff2ba3e6d84ccc0313958
Roddick Mujib <1442764@stu.tmc.ac.uk>
2022-10-05 10:19:01 +0100

    Update calculator.php

--- calculator-project-php/calculator.php
+++ calculator-project-php/calculator.php
@@ -7,54 +7,41 @@
     <link href="/css/bootstrap.min.css" rel="stylesheet">
 
     <body>
+
+    <form action="calculator.php" method="get">
+        <p>First number: <input type="text" name="num1"></p>
+        <p>Second number: <input type="text" name="num2"></p>
+        <p>Operator: <input type="text" name="operator"></p>
+        <input type="submit">
+    </form>
+
         <?php
-            $name_and_age = array(
-                array('Name' => 'Mitchell', 'Age' => 17),
-                array('Name' => 'Derron', 'Age' => 17),
-                array('Name' => 'Omarion', 'Age' => 19),
-                array('Name' => 'Ela', 'Age' => 17),
-                array('Name' => 'Jake', 'Age' => 18),
-                array('Name' => 'Alex', 'Age' => 17),
-                array('Name' => 'Rana', 'Age' => 19),
-                array('Name' => 'Rehman', 'Age' => 19),
-                array('Name' => 'Nour', 'Age' => 19),
-                array('Name' => 'Temi', 'Age' => 18),
-                array('Name' => 'Noman', 'Age' => 18),
-                array('Name' => 'Taylor', 'Age' => 18),
-                array('Name' => 'Roddick', 'Age' => 17),
-                array('Name' => 'Elizabete', 'Age' => 17),
-            );
-            
-            echo "<p>Sorted by name:</p>";
-            $key_values = array_column($name_and_age, 'Name');
-            array_multisort($key_values, SORT_ASC, $name_and_age);
+            <?php
+        if(empty ($_GET["num1"]) and empty ($_GET["num2"])) {
+            echo "";
+        }
 
-            foreach($name_and_age as $name_sorted) {
-                echo "&bull;", "NAME: ".implode('    |    AGE: ', $name_sorted) ."<br>";
+         $operator = $_GET["operator"];
+         switch($operator) {
+             case "+": echo "Result: ". $_GET["num1"] + $_GET["num2"];
+             break;
+             case "-": echo "Result: ". $_GET["num1"] - $_GET["num2"];
+             break;
+             case "*": echo "Result: ". $_GET["num1"] * $_GET["num2"];
+             break;
+             case "/": echo "Result: ". $_GET["num1"] / $_GET["num2"];
+             break;
+             default: echo "No data has been input";
+         }
+        echo "<br> <br>";
+        for($i = 1; $i < 6; $i++) {
+            echo "<dt> multiplications of $i";
+
+            for ($j = 1; $j < 6; $j++) {
+                $total = $i * $j;
+                echo "<dd> $i x $j = $total";
             }
-
-            echo "<br>";
-
-            echo "<p>Sorted by age:</p>";
-            $key_values = array_column($name_and_age, 'Age');
-            array_multisort($key_values, SORT_ASC, $name_and_age);
-
-            foreach($name_and_age as $age_sorted) {
-                echo "&bull;", "NAME: ".implode('   |   AGE: ', $age_sorted) ."<br>";
-            }
-
-            $culm_var = 0;
-            $name_and_age_count = count($name_and_age);
-
-            echo "<br>";
-
-            for($i =2; $i < $name_and_age_count; $i++){
-                $culm_var += $name_and_age[$i]["Age"];
-            }
-
-            $average_age = $culm_var / $name_and_age_count;
-
-            echo "Average age is: " .round($average_age, 1);
+        }
             
         ?>
 
