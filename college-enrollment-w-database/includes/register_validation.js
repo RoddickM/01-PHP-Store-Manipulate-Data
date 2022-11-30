75250967b8e73f0693664c2bcc77094bfa993649
RoddickM <98747226+RoddickM@users.noreply.github.com>
2022-11-30 14:53:01 +0000

    Validation testing (partially working)

--- college-enrollment-w-database/includes/register_validation.js
+++ college-enrollment-w-database/includes/register_validation.js
@@ -1,5 +1,9 @@
 const form = document.getElementById('form');
-const firstName = document.getElementById('first_name')
+const firstName = document.getElementById('first_name');
+const lastName = document.getElementById('last_name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -25,18 +29,52 @@ const setSuccess = element => {
     inputControl.classList.remove('error');
 };
 
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
 const validateInputs = () => {
-    const firstName = firstName.value.trim();
-    var letters = /^[A-Za-z]+$/
+    const firstNameValue = firstName.value.trim();
+    const lastNameValue = lastName.value.trim();
+    // const emailValue = email.value.trim();
+    // const passwordValue = password.value.trim();
+    // const password2Value = password2.value.trim();
 
-    if (firstName === ''){
-        setError(firstName, 'Please enter your first name');
+    if(firstNameValue === '') {
+        setError(firstName, 'First name is required');
+    } else {
+        setSuccess(firstName);
     }
-    else if (firstName.value.match(letters)){
-        setError(firstName, 'Please enter a valid first name');
+
+    if(lastNameValue === '') {
+        setError(lastName, 'Last name is required')
+    } else {
+        setSuccess(lastName)
     }
-    else {
-        setSuccess(email);
-    }
+
+    // if(emailValue === '') {
+    //     setError(email, 'Email is required');
+    // } else if (!isValidEmail(emailValue)) {
+    //     setError(email, 'Provide a valid email address');
+    // } else {
+    //     setSuccess(email);
+    // }
+
+    // if(passwordValue === '') {
+    //     setError(password, 'Password is required');
+    // } else if (passwordValue.length < 8 ) {
+    //     setError(password, 'Password must be at least 8 character.')
+    // } else {
+    //     setSuccess(password);
+    // }
+
+    // if(password2Value === '') {
+    //     setError(password2, 'Please confirm your password');
+    // } else if (password2Value !== passwordValue) {
+    //     setError(password2, "Passwords doesn't match");
+    // } else {
+    //     setSuccess(password2);
+    // }
+
 };
-
