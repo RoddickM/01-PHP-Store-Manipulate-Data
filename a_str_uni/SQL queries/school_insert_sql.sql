6894fe9d36baa0d87f4d8cb6eabbd99e4d652d2b
RoddickM <98747226+RoddickM@users.noreply.github.com>
2022-12-15 11:01:04 +0000

    Deleting duplicate file that already has its own repo

--- a_str_uni/SQL queries/school_insert_sql.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO school (school_id, school_name, school_type) VALUES
-(101, 'Primary School A', 'Primary School'),
-(102, 'Primary School B', 'Primary School'),
-(103, 'Primary School C', 'Primary School'),
-(104, 'Primary School D', 'Primary School'),
-(105, 'Secondary School A', 'Secondary School'),
-(106, 'Secondary School B', 'Secondary School')
